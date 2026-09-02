865167bc62df588aea9492f5dfbeff9f399d0f6a
artinasd <128320019+artinasd@users.noreply.github.com>
2026-09-02 21:36:33 +0330

    revert: remove backend task start endpoint

--- task-service/src/main/java/com/task_service/task_service/controller/TaskController.java
+++ task-service/src/main/java/com/task_service/task_service/controller/TaskController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.AccessDeniedException;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -21,7 +22,7 @@ public class TaskController {
     TaskService taskService;
 
     @PostMapping("add")
-    public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO taskDTO) {
+    public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO taskDTO) throws NoSuchAlgorithmException {
         return new ResponseEntity<>(taskService.createTask(taskDTO), HttpStatus.CREATED);
     }
 
@@ -46,12 +47,7 @@ public class TaskController {
     @PatchMapping("edit/{taskCode}")
     public ResponseEntity<TaskDTO> editTask(@PathVariable String taskCode,
                                             @RequestBody TaskDTO dto) throws AccessDeniedException {
-        return new ResponseEntity<>(taskService.editTask(taskCode, dto), HttpStatus.OK);
-    }
-
-    @PostMapping("start/{taskCode}")
-    public ResponseEntity<TaskDTO> startTask(@PathVariable String taskCode) throws AccessDeniedException {
-        return new ResponseEntity<>(taskService.startTask(taskCode), HttpStatus.OK);
+        return new ResponseEntity<>(taskService.editTask(taskCode, dto), HttpStatus.OK); // TODO : there is no need to unitCode
     }
 
     @DeleteMapping("delete/{taskCode}")
