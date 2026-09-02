e980451252a853284f2112f08fd2b430b18afe98
artinasd <128320019+artinasd@users.noreply.github.com>
2026-09-02 17:52:00 +0330

    feat(backend): expose global roles

--- task-service/src/main/java/com/task_service/task_service/service/RoleService.java
+++ task-service/src/main/java/com/task_service/task_service/service/RoleService.java
@@ -3,13 +3,16 @@ package com.task_service.task_service.service;
 import com.task_service.task_service.dto.RoleDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface RoleService {
+
+    List<RoleDTO> getAllRoles();
 
     RoleDTO createRole(RoleDTO roleDTO);
 
     RoleDTO editRole(RoleDTO roleDTO);
 
     boolean deleteRole(RoleDTO roleDTO);
-
 }
